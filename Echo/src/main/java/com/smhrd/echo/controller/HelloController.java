ff7e2723898e91b5e906e6dc80ab7959fc932825
서정선 <slowarirang@gmail.com>
2023-01-09 09:23:11 +0900

    초기 환경 구축

---
+++ Echo/src/main/java/com/smhrd/echo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.smhrd.echo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world~!";
+    }
+    
+}
