3ec5485bce6663782ef42d875694b42edddeecc7
m1otic <parkminook16@naver.com>
2023-02-16 11:10:39 +0900

    채팅 ㅇㅇ

--- Echo/src/main/java/com/smhrd/echo/config/WebSocketConfig.java
+++ Echo/src/main/java/com/smhrd/echo/config/WebSocketConfig.java
@@ -1,32 +1,26 @@
 package com.smhrd.echo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.smhrd.echo.controller.WebSocketHandler;
 
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer{
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-    	System.out.println("test");
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    private WebSocketHandler webSocketHandler = new WebSocketHandler();
+
+    public void WebsocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
     }
 
-
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
-    }
-    
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new FilterChannelInterceptor());
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/chatting/{roomNumber}").setAllowedOrigins("*");
     }
 }
+
--- Echo/src/main/java/com/smhrd/echo/controller/ChatController.java
+++ Echo/src/main/java/com/smhrd/echo/controller/ChatController.java
@@ -4,19 +4,23 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.smhrd.echo.model.ChatMessage;
 
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class ChatController {
 
-    @MessageMapping("/{seq}")
-    @SendTo("/topic/{seq}")
+    @MessageMapping("chat")
+    @SendTo("/topic/message")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
     	System.out.println("확인1:");
-    	System.out.println(chatMessage.getContent());
+    	//System.out.println(chatMessage.getContent());
         return chatMessage;
     }
 
@@ -24,7 +28,7 @@ public class ChatController {
     @SendTo("/topic/2")
     public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor){
     	System.out.println("확인2:");
-    	headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+    	//headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
         return chatMessage;
     }
 }
